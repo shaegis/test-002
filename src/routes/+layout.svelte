6a2183dd8d98b34bb981659e9385b0db4d85552a
shaegis <shaegis@gmail.com>
2025-02-13 17:11:51 +0900

    Add menus when logged in.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,25 @@
             </figure>
             <span>Charting Up</span>
         </div>
+        <ul class="menu">
+            {#if $page.data.user}
+                <li>
+                    <form method="POST" action="/account/progress">
+                        <button type="submit">Progress</button>
+                    </form>
+                </li>
+                <li>
+                    <form method="POST" action="/account/scales">
+                        <button type="submit">Scales</button>
+                    </form>
+                </li>
+                <li>
+                    <form method="POST" action="/account/protections">
+                        <button type="submit">Protections</button>
+                    </form>
+                </li>
+             {/if}
+        </ul>
         <ul class="auth">
             {#if $page.data.user}
                 <li>
@@ -51,6 +70,16 @@
     .logo {
         height: 40px;
         width: 52px;
+    }
+
+    .menu {
+        flex: 1;  /* nav 내에서 확장 */
+        display: flex;
+        justify-content: space-around; /* 가운데 정렬 */
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        gap: 1rem;
     }
 
     .auth {

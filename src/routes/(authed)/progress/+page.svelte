ecbe6d5eb46a7ce64f4208aa1f3b708ceaf1dbc0
shaegis <shaegis@gmail.com>
2025-02-19 21:40:19 +0900

    {#snippet} struggle. Fail to radioGroup & checkboxGroup. Commnet out.

--- src/routes/(authed)/progress/+page.svelte
+++ src/routes/(authed)/progress/+page.svelte
@@ -3,7 +3,7 @@
 
     let { data }: { data: PageServerData } = $props();
 
-    const psychological_symptoms = [
+    const psychologicalSymptoms = [
         // pychological
         { name: 'depressive', label: 'depressive' },
         { name: 'lack_of_drive', label: 'lack of drive' },
@@ -15,14 +15,14 @@
         { name: 'impulsive', label: 'impulsive' },
     ];
 
-    const somatic_symptoms = [
+    const somaticSymptoms = [
         // autonomic
         { name: 'heating_sense', label: 'heating sense' },
         { name: 'sweating', label: 'sweating' },
         { name: 'impending_death', label: 'impending death' },
         // head & neck
-        { name: 'concentation', label: 'concentation' },
-        { name: 'headache_migrain', label: 'headache/migrain' },
+        { name: 'concentration', label: 'concentration' },
+        { name: 'headacheMigraine', label: 'headache/migraine' },
         { name: 'blepharospasm', label: 'blepharospasm' },
         { name: 'tinnitus', label: 'tinnitus' },
         { name: 'vertigo', label: 'vertigo' },
@@ -32,7 +32,7 @@
         { name: 'heaviness', label: 'heaviness' },
         { name: 'dyspnea', label: 'dyspnea' },
         // abdomen
-        { name: 'nasea', label: 'nasea' },
+        { name: 'nausea', label: 'nausea' },
         { name: 'indigestion', label: 'indigestion' },
     ];
 
@@ -44,12 +44,13 @@
         { value: 'aggrevated', label: 'aggrevated' }
     ];
 
-    const percent_range = Array.from({ length: 10 }, (_, i) => ({
+    const percentRange = Array.from({ length: 10 }, (_, i) => ({
         value: `${90 - i * 10}`,
         label: `${90 - i * 10}`
     }));
 
-    const time_periods = ['awake', 'morning', 'noon', 'afternoon', 'dusk', 'evening', 'night', 'before bed', 'sleep'];
+    const timePeriods = ['awake', 'morning', 'noon', 'afternoon', 'dusk', 'evening', 'night', 'before bed', 'sleep'];
+    const sxProgress = ['persist', 'aggravated', 'improved', 'resolved'];
 
     let form = $state({
         psychological: {} as Record<string, string>,
@@ -62,9 +63,7 @@
             intensity: ''
         },
         suicidal: {
-            idea: false,
-            plan: false,
-            attampt: false,
+            type: [] as string[],
             methods: [] as string[],
             otherMethod: '',
             when: {
@@ -73,183 +72,154 @@
             }
         },
         homocidal: {
-            idea: false,
-            plan: false,
-            attempt: false
-        }
+            type: [] as string[]
+        },
     });
 </script>
 
+{#snippet symptoms(symptom, symptomGroup)}
+    <div class="flex items-center gap-4">
+        <span>{symptom.label}</span>
+        {#each options as option}
+            <label>
+                <input type="radio" value={option.value} bind:group={form[symptomGroup][symptom.name]} />
+                {option.label}
+            </label>
+        {/each}
+    </div>
+{/snippet}
+
+<!--
+{#snippet radioGroup(options, bindTo, labelPrefix = '')}
+    {#each options as option}
+        <label>
+            <input type="radio" value={option} bind:group={bindTo} />
+            {labelPrefix}{option}
+        </label>
+    {/each}
+{/snippet}
+usage: {@render radioGroup(timePeriods, form.occursIn)}
+
+{#snippet checkboxGroup(options, bindTo)}
+    {#each options as option}
+        <label>
+            <input type="checkbox" value={option} bind:group={bindTo} />
+            {option}
+        </label>
+    {/each}
+{/snippet}
+usage: {@render checkboxGroup(['idea', 'plan', 'attempt'], form.suicidal.type)}
+-->
+
 <article class="prose">
-    <h1>{data.user.username} Progress Notes</h1>
-    <p>You are in the authed & welcomed.</p>
-    <p>Your user ID is {data.user.id}.</p>
+<h1>{data.user.username} Progress Notes</h1>
+<p>You are in the authed & welcomed.</p>
+<p>Your user ID is {data.user.id}.</p>
 
-    <form>
-        <h2>Subjective</h2>
-        <section>
-            <h3>Symptoms</h3>
-            <fieldset>
-                <legend>Psychological Symptoms</legend>
-                {#each psychological_symptoms as symptom}
-                    <div class="flex items-center gap-4">
-                        <span>{symptom.label}</span>
-                        {#each options as option}
-                            <label>
-                                <input
-                                    type="radio"
-                                    value={option.value}
-                                    bind:group={form.psychological[symptom.name]}
-                                />
-                                <span>{option.label}</span>
-                            </label>
-                        {/each}
-                    </div>
-                {/each}
-            </fieldset>
+<form>
+<h2>Subjective</h2>
+    <h3>Symptoms</h3>
+    <fieldset>
+        <legend>Psychological Symptoms</legend>
+        {#each psychologicalSymptoms as symptom}
+            {@render symptoms(symptom, "psychological")}
+        {/each}
+    </fieldset>
 
-            <fieldset>
-                <legend>Somatic Symptoms</legend>
-                {#each somatic_symptoms as symptom}
-                    <div class="flex items-center gap-4">
-                        <span>{symptom.label}</span>
-                        {#each options as option}
-                            <label>
-                                <input
-                                    type="radio"
-                                    value={option.value}
-                                    bind:group={form.somatic[symptom.name]}
-                                />
-                                <span>{option.label}</span>
-                            </label>
-                        {/each}
-                    </div>
-                {/each}
-            </fieldset>
+    <fieldset>
+        <legend>Somatic Symptoms</legend>
+        {#each somaticSymptoms as symptom}
+            {@render symptoms(symptom, "somatic")}
+        {/each}
+    </fieldset>
 
-            <fieldset>
-                <legend>mainly occures in/at</legend>
-                <div class="flex items-center gap-4">
-                    {#each time_periods as period}
-                        <label>
-                            <input
-                                type="radio"
-                                value={period}
-                                bind:group={form.occursIn}
-                            />
-                            {period}
-                        </label>
+    <fieldset>
+        <legend>mainly occures in/at</legend>
+        <div class="flex items-center gap-4">
+            {#each timePeriods as period}
+                <label>
+                    <input type="radio" value={period} bind:group={form.occursIn} />
+                    {period}
+                </label>
+            {/each}
+        </div>
+    </fieldset>
+
+    <fieldset>
+        <legend>Attack</legend>
+        <div class="flex items-center gap-4">
+            {#each sxProgress as progress}
+                <label>
+                    <input type="radio" value={progress} bind:group={form.attack.type} />
+                    {progress}
+                </label>
+            {/each}
+
+            <input
+                type="number" bind:value={form.attack.frequency} placeholder="times" />
+
+            {#each ['day', 'week'] as unit}
+                <label>
+                    <input type="radio" value={unit} bind:group={form.attack.unit} />
+                    / {unit}
+                </label>
+            {/each}
+
+            <label>
+                <select bind:value={form.attack.intensity}>
+                    {#each percentRange as pRange}
+                        <option value={pRange.value}>{pRange.label}</option>
                     {/each}
-                </div>
-            </fieldset>
+                </select>
+                % intensity
+            </label>
+        </div>
+    </fieldset>
 
-            <fieldset>
-                <legend>Attack</legend>
-                <div class="flex items-center gap-4">
-                    {#each ['persist', 'aggrevated', 'improved', 'resolved'] as type}
-                        <label>
-                            <input
-                                type="radio"
-                                value={type}
-                                bind:group={form.attack.type}
-                            />
-                            {type}
-                        </label>
-                    {/each}
+    <fieldset>
+        <legend>Suicidal idea</legend>
+        <div class="flex items-center gap-4">
+            {#each ['idea', 'plan', 'attempt'] as type}
+                <label>
+                    <input type="checkbox" bind:group={form.suicidal.type} />
+                    {type}
+                </label>
+            {/each}
+        </div>
 
-                    <input
-                        type="number"
-                        bind:value={form.attack.frequency}
-                        placeholder="times"
-                    />
+        <div class="flex items-center gap-4">
+            {#each ['hanging', 'poisoning', 'knife'] as method}
+                <label>
+                    <input type="checkbox" bind:group={form.suicidal.methods} />
+                    {method}
+                </label>
+            {/each}
+            <input type="text" bind:value={form.suicidal.otherMethod} placeholder="Other method" />
+        </div>
 
-                    {#each ['day', 'week'] as unit}
-                        <label>
-                            <input
-                                type="radio"
-                                value={unit}
-                                bind:group={form.attack.unit}
-                            />
-                            /{unit}
-                        </label>
-                    {/each}
+        <div class="flex items-center gap-4">
+            <input type="number" bind:value={form.suicidal.when.amount} placeholder="1 day/month/year ago" />
+            {#each ['days', 'months', 'years'] as unit}
+                <label>
+                    <input type="radio" value={unit} bind:group={form.suicidal.when.unit} />
+                    {unit} ago
+                </label>
+            {/each}
+        </div>
+    </fieldset>
 
-                    <label>
-                        <select bind:value={form.attack.intensity}>
-                            {#each percent_range as p_range}
-                                <option value={p_range.value}>{p_range.label}</option>
-                            {/each}
-                        </select>
-                        % intensity
-                    </label>
-                </div>
-            </fieldset>
+    <fieldset>
+        <legend>Homocidal idea</legend>
+        <div class="flex items-center gap-4">
+            {#each ['idea', 'plan', 'attempt'] as type}
+                <label>
+                    <input type="checkbox" bind:group={form.homocidal.type} />
+                    {type}
+                </label>
+            {/each}
+        </div>
+    </fieldset>
 
-            <fieldset>
-                <legend>Suicidal idea</legend>
-                <div class="flex items-center gap-4">
-                    {#each ['idea', 'plan', 'attempt'] as type}
-                        <label>
-                            <input
-                                type="checkbox"
-                                bind:checked={form.suicidal.idea}
-                            />
-                            {type}
-                        </label>
-                    {/each}
-                </div>
-
-                <div class="flex items-center gap-4">
-                    {#each ['hanging', 'poisoning', 'knife'] as method}
-                        <label>
-                            <input
-                                type="checkbox"
-                                value={method}
-                                bind:group={form.suicidal.methods}
-                            />
-                            {method}
-                        </label>
-                    {/each}
-                    <input
-                        type="text"
-                        bind:value={form.suicidal.otherMethod}
-                        placeholder="Other method"
-                    />
-                </div>
-
-                <div class="flex items-center gap-4">
-                    <input
-                        type="number"
-                        bind:value={form.suicidal.when.amount}
-                        placeholder="1 day/month/year ago"
-                    />
-                    {#each ['days', 'months', 'years'] as unit}
-                        <label>
-                            <input
-                                type="radio"
-                                value={unit}
-                                bind:group={form.suicidal.when.unit}
-                            />
-                            {unit} ago
-                        </label>
-                    {/each}
-                </div>
-            </fieldset>
-
-            <fieldset>
-                <legend>Homocidal idea</legend>
-                <div class="flex items-center gap-4">
-                    {#each ['idea', 'plan', 'attempt'] as type}
-                        <label>
-                            <input
-                                type="checkbox"
-                                bind:checked={form.homocidal.idea}
-                            />
-                            {type}
-                        </label>
-                    {/each}
-                </div>
-            </fieldset>
-        </section>
-    </form>
+    <h3>Interpersonal Relations</h3>
+</form>
 </article>
